0be6a6d03fe16c37bd3ac767d046d03b1e691669
killua <killua@desktop>
2024-03-12 17:05:22 +0000

    getting, creating some products

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+import json
+
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Product(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+
+with open("data/products.json") as f:
+    products = json.load(f)
+    f.close()
 
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
+@app.get("/products")
+def get_products():
+    return {"data": products}
 
+@app.get("/product/{id}")
+def get_product(id: int):
+    for p in products:
+        if p["id"] == id:
+            product = p
+    print(product)
+    return {"product": product}
+
+
+@app.post("/create")
+def create_product(new_product: Product):
+    # products.append(new_product)
+    print(new_product)
+    return {"message": new_product}
