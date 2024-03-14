18a7fabec7cfd27530b6a009b367352f0553b61b
killua <killua@desktop>
2024-03-14 02:56:41 +0000

    modify response status code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, Response, status
+# from fastapi.params import Body
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -9,8 +9,9 @@ app = FastAPI()
 class Product(BaseModel):
     id: int
     name: str
-    description: str
+    description: str = ""
     price: float
+    stock: int
 
 with open("data/products.json") as f:
     products = json.load(f)
@@ -26,16 +27,21 @@ def get_products():
     return {"data": products}
 
 @app.get("/product/{id}")
-def get_product(id: int):
+def get_product(id: int, response: Response):
+    product = None
     for p in products:
         if p["id"] == id:
             product = p
-    print(product)
+
+    if not product:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"product with id {id} was not found"}
     return {"product": product}
 
 
 @app.post("/create")
 def create_product(new_product: Product):
-    # products.append(new_product)
-    print(new_product)
+    print(new_product.model_dump())
+    products.append(new_product.model_dump())
+    print(products)
     return {"message": new_product}
